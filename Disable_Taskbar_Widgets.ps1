f04112434933b34c116908c9188e5223718778c6
Cole Hartnett <44676398+Elochar@users.noreply.github.com>
2026-01-28 16:11:15 -0700

    Add files via upload

---
+++ Disable_Taskbar_Widgets.ps1
@@ -0,0 +1,7 @@
+﻿# -- 1. Disable Taskbar Widgets --
+$RegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+New-ItemProperty -Path $RegistryPath -Name "TaskbarDa" -Value 0 -PropertyType DWord -Force | Out-Null
+
+# -- 5. Refresh Windows Explorer --
+Write-Host "Refreshing UI..." -ForegroundColor Green
+Stop-Process -Name explorer -Force
